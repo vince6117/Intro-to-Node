85031cabc2405fe07736fa459d4c879fb276672d
Vincent CT <vincent8658@gmail.com>
2021-01-24 23:20:50 +0800

    Initial Commit

---
+++ hello.js
@@ -0,0 +1,35 @@
+//console.log('Hello World');
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+
+
+http.createServer(function (req, res) {
+	//var q = url.parse(req.url, true).query;
+	var q = url.parse(req.url, true);
+	//console.log(q.pathname);
+	var filename = "." + q.pathname;
+	//console.log(filename);
+	if (filename == './') {filename = './index'};
+
+	filename = filename + ".html";
+	console.log(filename);
+	//fs.readFile('index.html', function(err, data){
+	fs.readFile(filename, function(err, data){
+		if (err) {
+			res.writeHead(404,{'Content-Type': 'text/html'});
+			return res.end("404 Not Found");
+		}
+
+		res.writeHead(200,{'Content-Type': 'text/html'});
+	//var q = url.parse(req.url, true).query;
+	//var dates = q.year + " " + q.month;
+	//res.end(dates);
+		res.write(data);
+		console.log("...Incoming Request: " + req.url);
+		return res.end();
+	})
+}).listen(8080);
+
+console.log("Server Listening on Port 8080...");
